65919d3bcf36ad63521d0b81fe71a7df40c6bcf3
amovfx <45839100+amovfx@users.noreply.github.com>
2022-09-08 10:48:39 -0700

    changed repo name

--- verify_error_msg.sh
+++ verify_error_msg.sh
@@ -20,13 +20,13 @@ export MASTER_CHAIN_MERGE_TEST_OUT=/tmp/master_results.txt
 
 
 cd $BITCOIN_GIT_DIR
-git checkout argument-collision-warning
+git checkout network-argument-collision-verbosity
 configure #these are my functions, you can just run ./configure
 build #these are my functions, you can just run make -j N
 
 cd $BITCOIN_BUILD_DIR
-CHAIN_MERGE_TEST_OUT=/tmp/argument-collision-warning_results.txt ${BITCOIN_BUILD_DIR}/src/test/test_bitcoin --run_test=util_tests/util_ChainMerge
-export BRANCH_CHAIN_MERGE_TEST_OUT=/tmp/argument-collision-warning_results.txt
+CHAIN_MERGE_TEST_OUT=/tmp/network-argument-collision-verbosity_results.txt ${BITCOIN_BUILD_DIR}/src/test/test_bitcoin --run_test=util_tests/util_ChainMerge
+export BRANCH_CHAIN_MERGE_TEST_OUT=/tmp/network-argument-collision-verbosity_results.txt
 
 
 #diff ${MASTER_CHAIN_MERGE_TEST_OUT} ${BRANCH_CHAIN_MERGE_TEST_OUT}
